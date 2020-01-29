3d83fc855d3d0cdc27725e57e2afd0f4646917d4
Simão Meyrer <57023157+tsimameyrer@users.noreply.github.com>
2020-01-29 13:09:39 -0300

    Delete for refatoring

--- controllers/Services/contatosAPIService.js
+++
@@ -1,9 +0,0 @@
-angular.module("listaTelefonica").factory("contatosAPI", function ($http) {
-    var _getContatos = function () {
-        return $http.get("http://localhost:3412/contatos")
-    }
-    
-    return {
-        getContatos: _getContatos
-    }
-})
